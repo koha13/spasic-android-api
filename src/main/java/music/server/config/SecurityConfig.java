2cada83f071099fd8d16c922b81b8068b3238546
khoa <policekhoa13101998@gmail.com>
2020-06-28 23:07:31 +0700

    Change authenticate for signup

--- src/main/java/music/server/config/SecurityConfig.java
+++ src/main/java/music/server/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/image/**").permitAll();
         http.authorizeRequests().antMatchers("/song/**").permitAll();
         http.authorizeRequests().antMatchers("/scan").authenticated();
-        http.authorizeRequests().antMatchers("/auth/signup").authenticated();
+        http.authorizeRequests().antMatchers("/auth/signup").permitAll();
         http.authorizeRequests().antMatchers("/auth/changepass").authenticated();
         http.authorizeRequests().antMatchers("/auth/**").permitAll().anyRequest().authenticated();
 
